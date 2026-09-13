3ec67507a05c63b11f8b6d2201c42a5047b10430
Phoenix2994 <filippobianco2@gmail.com>
2026-09-13 18:16:39 +0200

    Auto-generated commit

--- chunk-KZPOE7PJ.js
+++
@@ -1,1 +0,0 @@
-import{K as a,Z as l}from"./chunk-UR5O7PSS.js";var n=class e{selectedTeamIdSignal=l(null);selectedTeamId=this.selectedTeamIdSignal.asReadonly();select(t){this.selectedTeamIdSignal.set(t)}static \u0275fac=function(i){return new(i||e)};static \u0275prov=a({token:e,factory:e.\u0275fac,providedIn:"root"})};export{n as a};
